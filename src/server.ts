e04ee319fea1dcdc98eb10265e31862aea5d32c1
pantho0 <panthomashreky6@gmail.com>
2025-09-02 12:32:47 +0600

    Initialized basic backend

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import app from "./app"
+import config from "./app/config";
+
+
+
+async function main() {
+    try {
+        await mongoose.connect(config.db_url as string);
+        app.listen(config.port, () => {
+            console.log(`Our Kutum listening on port ${config.port}`)
+          })
+    } catch (error) {
+        console.log(error)
+    }  
+  }
+
+main()
+
+
+  
