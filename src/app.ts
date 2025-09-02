68aae309c3554baf1075f920f287a646a2cfce96
pantho0 <panthomashreky6@gmail.com>
2025-09-02 12:44:50 +0600

    added script for running the backend

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
-import express, { Application, Request, Response } from 'express'
-const app:Application = express()
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to Our Kutum Restaurant 🍔');
+});
 
-app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!')
-})
-
-export default app
+export default app;
