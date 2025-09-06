607a1afe34862cb7d8e8fbd289a839beb431f7fb
pantho0 <panthomashreky6@gmail.com>
2025-09-06 13:07:01 +0600

    fixed jwt expires issue

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -6,6 +6,7 @@ import { createToken } from './auth.utils';
 import config from '../../config';
 
 const loginUser = async (payload: TLogin) => {
+  console.log(config.jwt_acces_exp);
   const user = await User.isUserExist(payload.email);
   if (!user) {
     throw new AppError(status.NOT_FOUND, 'User not found');
