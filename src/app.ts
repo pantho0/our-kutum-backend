86c85811e1c58d40b707341a5a32ce2d4fcad19b
Mashraki Toki Tahmid <panthomashreky6@gmail.com>
2025-09-10 13:49:03 +0600

    added cloudinary image upload api

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,19 @@ import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import router from './app/routes';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import { uploadImage } from './app/utils/uploadImage';
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
 
 app.use('/api/v1', router);
+
+app.post('/api/v1/cloudinary', async (req, res) => {
+  const { image } = req.body;
+  const result = await uploadImage(image);
+  res.status(200).json(result);
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Our Kutum Restaurant 🍔');
